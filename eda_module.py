5e91ee31143d4661593b41f971f3d44eafe9dd9c
Andrew Cole <andrewcole.817@gmail.com>
2020-05-06 15:55:08 -0500

    Complete EDA for all categorical Features

--- eda_module.py
+++ eda_module.py
@@ -94,7 +94,7 @@ def plot_age_dist(df):
     g = df.copy()
     g = g.groupby('seniorcitizen')['churn'].value_counts().to_frame()
     g = g.rename({'churn':'pct_total'}, axis = 1).reset_index()
-    g['pct_total'] = g['pct_total']/len(df)
+    g['pct_total'] = (g['pct_total']/len(df)) * 100
     t = sns.barplot('seniorcitizen', y = 'pct_total', hue = 'churn', data = g)
     t.set_title('Churn % by Age', fontsize = 30, fontweight = 'bold')
     t.set_xlabel('')
@@ -127,7 +127,7 @@ def plot_partner_dependents(df):
     x = df.copy()
     x = x.groupby('partner')['churn'].value_counts().to_frame()
     x = x.rename({'churn':'pct_total'}, axis = 1).reset_index()
-    x['pct_total'] = x['pct_total']/len(df)
+    x['pct_total'] = (x['pct_total']/len(df)) * 100
     u = sns.barplot('partner', y = 'pct_total', hue = 'churn', data = x)
     u.set_title('Churn % by Partner', fontweight = 'bold', fontsize = 30)
     u.set(xticklabels = ['No Partner', 'Partner'])
@@ -138,7 +138,7 @@ def plot_partner_dependents(df):
     y = df.copy()
     y = y.groupby('dependents')['churn'].value_counts().to_frame()
     y = y.rename({'churn':'pct_total'}, axis = 1).reset_index()
-    y['pct_total'] = y['pct_total']/len(df)
+    y['pct_total'] = (y['pct_total']/len(df)) * 100
     v = sns.barplot('dependents', y = 'pct_total', hue = 'churn', data = y)
     v.set_title('Churn % by Dependents', fontweight = 'bold', fontsize = 30)
     v.set(xticklabels = ['No Dependents', 'Dependents'])
@@ -169,7 +169,7 @@ def plot_services(df):
     z = df.copy()
     z = z.groupby('phoneservice')['churn'].value_counts().to_frame()
     z = z.rename({'churn':'pct_total'}, axis = 1).reset_index()
-    z['pct_total'] = z['pct_total']/len(df)
+    z['pct_total'] = (z['pct_total']/len(df)) * 100
     a = sns.barplot('phoneservice', y = 'pct_total', hue = 'churn', data = z)
     a.set_title('% Churn by Phone Service', fontsize = 30, fontweight = 'bold')
     a.set(xticklabels = ['No Phone', 'Phone'])
@@ -178,14 +178,6 @@ def plot_services(df):
     
     
     plt.subplot(233)
-#     aa = df.copy()
-#     aa = aa.groupby(['churn', 'phoneservice'])['monthlycharges'].mean().to_frame()
-#     aa = aa.reset_index()
-#     r = sns.barplot('phoneservice', y = 'monthlycharges', hue = 'churn', data = aa)
-#     r.set_title('Phone Service - Mean Monthly Charges', fontsize = 30, fontweight = 'bold')
-#     r.set_xlabel('')
-#     r.set(xticklabels = ['No Phone', 'Phone'])
-#     r.set_ylabel('Mean Monthly Charge ($)', fontweight = 'bold', fontsize = 20)
     v1 = sns.violinplot('phoneservice', 'monthlycharges', 'churn', df, split = True)
     v1.set_title('Violin Plot: Monthly Charges by Phone Service', fontsize = 30, fontweight = 'bold')
     v1.set_xlabel('')
@@ -202,7 +194,7 @@ def plot_services(df):
     bb = df.copy()
     bb = bb.groupby('multiplelines')['churn'].value_counts().to_frame()
     bb = bb.rename({'churn':'pct_total'}, axis = 1).reset_index()
-    bb['pct_total'] = bb['pct_total']/len(df)
+    bb['pct_total'] = (bb['pct_total']/len(df)) * 100
     c = sns.barplot('multiplelines', y = 'pct_total', hue = 'churn', data = bb)
     c.set(xticklabels = ['Singular Line', 'No Phone Service', 'Multiple Lines'])
     c.set_title('')
@@ -218,3 +210,217 @@ def plot_services(df):
     v.set(xticklabels = ['No Phone Service', 'Singular Line', 'Multiple Lines'])
     
     fig.suptitle('Phone Services -  Line Quantity', fontweight = 'bold', fontsize = 40)
+    
+def plot_internet_services(df):
+    
+    copy = df.copy()
+    fig = plt.figure(figsize = (30, 10))
+
+    plt.subplot(131)
+    plt.pie(copy.internetservice.value_counts(), labels = ['Fiber Optic', 'DSL', 'No Internet'], autopct = '%.1f%%', textprops = {'fontsize':20, 'fontweight': 'bold'}, startangle = 180)
+    plt.title('Internet Service Composition of Customers', fontweight = 'bold', fontsize = 30)
+    
+    plt.subplot(132)
+    copy = copy.groupby('internetservice')['churn'].value_counts().to_frame()
+    copy = copy.rename({'churn':'pct_total'}, axis = 1).reset_index()
+    copy['pct_total'] = (copy['pct_total']/len(df)) * 100
+    d = sns.barplot('internetservice', y = 'pct_total', hue = 'churn', data = copy)
+    d.set_title('% Churn by Internet Service', fontweight= 'bold', fontsize = 30)
+    d.set_xlabel('')
+    d.set_ylabel('% of Customers', fontweight = 'bold', fontsize = 20)
+    d.set(xticklabels = ['DSL', 'Fiber Optic', 'No Internet Service'])
+    
+    plt.subplot(133)
+    e = sns.violinplot('internetservice', 'monthlycharges', 'churn', df, split = True)
+    e.set_title('Violin Plot: Monthly Charges by Internet Service', fontweight = 'bold', fontsize = 30)
+    e.set_xlabel('')
+    e.set(xticklabels = ['DSL', 'Fiber Optic', 'No Internet Service'])
+    e.set_ylabel('Monthly Charges($)', fontweight = 'bold', fontsize = 30)
+
+    fig.tight_layout()
+    
+
+def plot_services(df):
+    copy = df[df.internetservice != 'No']
+    
+    fig = plt.figure(figsize = (40, 15))
+    
+    plt.subplot(261)
+    plt.pie(copy.onlinesecurity.value_counts(), labels = ['Yes', 'No'], autopct = '%.1f%%', textprops = {'fontsize':20, 'fontweight':'bold'})
+    plt.title('Customers w/ Online Security', fontweight = 'bold', fontsize = 25)
+    
+    plt.subplot(262)
+    plt.pie(copy.onlinebackup.value_counts(), labels = ['Yes', 'No'], autopct = '%.1f%%', textprops = {'fontsize':20, 'fontweight':'bold'})
+    plt.title('Customers w/ Online Backup', fontweight = 'bold', fontsize = 25)
+    
+    plt.subplot(263)
+    plt.pie(copy.deviceprotection.value_counts(), labels = ['Yes', 'No'], autopct = '%.1f%%', textprops = {'fontsize':20, 'fontweight':'bold'})
+    plt.title('Customers w/ Device Protection', fontweight = 'bold', fontsize = 25)
+    
+    plt.subplot(264)
+    plt.pie(copy.techsupport.value_counts(), labels = ['Yes', 'No'], autopct = '%.1f%%', textprops = {'fontsize':20, 'fontweight':'bold'})
+    plt.title('Customers w/ Tech Support', fontweight = 'bold', fontsize = 25)
+    
+    plt.subplot(265)
+    plt.pie(copy.streamingtv.value_counts(), labels = ['Yes', 'No'], autopct = '%.1f%%', textprops = {'fontsize':20, 'fontweight':'bold'})
+    plt.title('Customers w/ Streaming TV', fontweight = 'bold', fontsize = 25)
+    
+    plt.subplot(266)
+    plt.pie(copy.streamingmovies.value_counts(), labels = ['Yes', 'No'], autopct = '%.1f%%', textprops = {'fontsize':20, 'fontweight':'bold'})
+    plt.title('Customers w/ Movie Streaming', fontweight = 'bold', fontsize = 25)
+    
+    plt.subplot(267)
+    copy1 = copy[copy.onlinesecurity == 'Yes']
+    plt.pie(copy1.churn.value_counts(), labels = ['No Churn', 'Churn'], autopct = '%.1f%%', textprops = {'fontsize':20, 'fontweight':'bold'})
+    plt.title('Online Security - Churn %', fontsize = 25, fontweight = 'bold')
+    
+    plt.subplot(268)
+    copy2 = copy[copy.onlinebackup == 'Yes']
+    plt.pie(copy2.churn.value_counts(), labels = ['No Churn', 'Churn'], autopct = '%.1f%%', textprops = {'fontsize':20, 'fontweight':'bold'})
+    plt.title('Online Backup - Churn %', fontsize = 25, fontweight = 'bold')
+    
+    plt.subplot(269)
+    copy3 = copy[copy.deviceprotection == 'Yes']
+    plt.pie(copy3.churn.value_counts(), labels = ['No Churn', 'Churn'], autopct = '%.1f%%', textprops = {'fontsize':20, 'fontweight':'bold'})
+    plt.title('Device Protection - Churn %', fontsize = 25, fontweight = 'bold')
+    
+    plt.subplot(2,6,10)
+    copy4 = copy[copy.techsupport == 'Yes']
+    plt.pie(copy4.churn.value_counts(), labels = ['No Churn', 'Churn'], autopct = '%.1f%%', textprops = {'fontsize':20, 'fontweight':'bold'})
+    plt.title('Tech Support - Churn %', fontsize = 25, fontweight = 'bold')
+    
+    plt.subplot(2,6,11)
+    copy5 = copy[copy.streamingtv == 'Yes']
+    plt.pie(copy5.churn.value_counts(), labels = ['No Churn', 'Churn'], autopct = '%.1f%%', textprops = {'fontsize':20, 'fontweight':'bold'})
+    plt.title('Streaming TV - Churn %', fontsize = 25, fontweight = 'bold')
+    
+    plt.subplot(2,6,12)
+    copy6 = copy[copy.streamingmovies == 'Yes']
+    plt.pie(copy6.churn.value_counts(), labels = ['No Churn', 'Churn'], autopct = '%.1f%%', textprops = {'fontsize':20, 'fontweight':'bold'})
+    plt.title('Streaming Movies - Churn %', fontsize = 25, fontweight = 'bold')
+    
+    plt.tight_layout()
+    
+    
+def plot_service_charges(df):
+    
+    fig, axes = plt.subplots(nrows = 1, ncols = 6, figsize = (40, 10), sharex = True, sharey = True)
+    
+    copy1 = df[df.onlinesecurity != 'No internet service']
+    a = sns.violinplot('onlinesecurity', 'monthlycharges', 'churn', copy1, split = True, ax = axes[0])
+    a.set_title('Online Security', fontsize = 30, fontweight = 'bold')
+    a.set_xlabel('Has Service', fontweight = 'bold', fontsize = 20)
+    a.set_ylabel('Monthly Charges ($)', fontweight = 'bold', fontsize = 20)
+    a.set_yticklabels(a.get_yticks(), size = 25, weight = 'bold')
+    a.legend(loc = 'upper left', )
+    a.legend_.set_title('Churn', prop = {'size': 20, 'weight':'bold'})
+
+    copy2 = df[df.onlinebackup != 'No internet service']
+    b = sns.violinplot('onlinebackup', 'monthlycharges', 'churn', copy2, split = True, ax = axes[1])
+    b.set_title('Online Backup', fontweight = 'bold', fontsize = 30)
+    b.set_xlabel('Has Service', fontweight = 'bold', fontsize = 20)
+    b.set_ylabel('')
+    b.legend_.remove()
+    
+    copy3 = df[df.deviceprotection != 'No internet service']
+    c = sns.violinplot('onlinebackup', 'monthlycharges', 'churn', copy3, split = True, ax = axes[2])
+    c.set_title('Device Protection', fontweight = 'bold', fontsize = 30)
+    c.set_xlabel('Has Service', fontweight = 'bold', fontsize = 20)
+    c.set_ylabel('')
+    c.legend_.remove()
+    
+    copy4 = df[df.techsupport != 'No internet service']
+    d = sns.violinplot('techsupport', 'monthlycharges', 'churn', copy3, split = True, ax = axes[3])
+    d.set_title('Tech Support', fontsize = 30, fontweight = 'bold')
+    d.set_xlabel('Has Service', fontsize = 20, fontweight = 'bold')
+    d.set_ylabel('')
+    d.legend_.remove()
+    
+    copy5 = df[df.streamingtv != 'No internet service']
+    e = sns.violinplot('streamingtv', 'monthlycharges', 'churn', copy4, split = True, ax = axes[4])
+    e.set_title('Streaming TV', fontsize = 30, fontweight = 'bold')
+    e.set_xlabel('Has Service', fontweight = 'bold', fontsize = 20)
+    e.set_ylabel('')
+    e.legend_.remove()
+    
+    copy5 = df[df.streamingmovies != 'No internet service']
+    f = sns.violinplot('streamingmovies', 'monthlycharges', 'churn', copy5, split = True, ax = axes[5])
+    f.set_title('Streaming Movies', fontweight = 'bold', fontsize = 30)
+    f.set_xlabel('Has Service', fontsize = 20, fontweight = 'bold')
+    f.set_ylabel('')
+    f.legend(loc = 'upper left')
+    f.legend_.set_title('Churn', prop = {'size': 20, 'weight':'bold'})
+    
+    
+def plot_contracts(df):
+    
+    copy = df.copy()
+    
+    plt.figure(figsize = (30, 10))
+    
+    plt.subplot(131)
+    plt.pie(copy.contract.value_counts(), labels = ['Monthly', '1-Year', '2-Year'], autopct = '%.1f%%', textprops = {'fontweight':'bold', 'fontsize': 20})
+    plt.title('Customer Contract Composition', fontweight = 'bold', fontsize = 30)
+    
+    plt.subplot(132)
+    plt.title('Churn % by Contract Type', fontsize = 30, fontweight = 'bold')
+    copy = copy.groupby('contract')['churn'].value_counts().to_frame()
+    copy = copy.rename({'churn':'pct_total'}, axis = 1).reset_index()
+    copy['pct_total'] = (copy['pct_total']/len(df)) * 100
+    a = sns.barplot('contract', y = 'pct_total', hue = 'churn', data = copy)
+    a.set_title('% Churn - Contract Type', fontsize = 30, fontweight = 'bold')
+    a.set(xticklabels = ['Monthly', '1-Year', '2-Year'])
+    a.set_xlabel('')
+    a.set_ylabel('% of Customers', fontweight = 'bold')
+    
+    plt.subplot(133)
+    b = sns.violinplot('contract', 'monthlycharges', 'churn', df, split = True)
+    b.set_title('Violin Plot: Monthly Charge - Contract Types', fontweight = 'bold', fontsize = 30)
+    b.set_xlabel('')
+    b.set_ylabel('Monthly Charges ($)', fontweight = 'bold', fontsize = 20)
+    b.set(xticklabels = ['Monthly', '1-Year', '2-Year'])
+    b.legend(loc = 'upper left')
+    b.legend_.set_title('Churn', prop = {'weight':'bold', 'size':20})
+    
+def plot_paperless(df):
+    
+    copy = df.copy()
+    
+    plt.figure(figsize = (30, 10))
+    
+    plt.subplot(131)
+    plt.pie(copy.paperlessbilling.value_counts(), labels = ['Paperless', 'Not Paperless'], autopct = '%.1f%%', textprops = {'fontweight':'bold', 'fontsize': 20})
+    plt.title('Customer Paperless Billing Composition', fontweight = 'bold', fontsize = 30)
+    
+    plt.subplot(132)
+    plt.title('Churn % by Billing Type', fontsize = 30, fontweight = 'bold')
+    copy = copy.groupby('paperlessbilling')['churn'].value_counts().to_frame()
+    copy = copy.rename({'churn':'pct_total'}, axis = 1).reset_index()
+    copy['pct_total'] = (copy['pct_total']/len(df)) * 100
+    a = sns.barplot('paperlessbilling', y = 'pct_total', hue = 'churn', data = copy)
+    a.set_title('% Churn - Paperless Billing', fontsize = 30, fontweight = 'bold')
+    a.set(xticklabels = ['Paperless', 'Not Paperless'])
+    a.set_xlabel('')
+    a.set_ylabel('% of Customers', fontweight = 'bold')
+    
+    plt.subplot(133)
+    b = sns.violinplot('paperlessbilling', 'monthlycharges', 'churn', df, split = True)
+    b.set_title('Violin Plot: Monthly Charge - Contract Types', fontweight = 'bold', fontsize = 30)
+    b.set_xlabel('')
+    b.set_ylabel('Monthly Charges ($)', fontweight = 'bold', fontsize = 20)
+    b.set(xticklabels = ['Paperless', 'Not Paperless'])
+    b.legend(loc = 'upper right')
+    b.legend_.set_title('Churn', prop = {'weight':'bold', 'size':20})
+    
+    plt.tight_layout()
+    
+    
+def plot_pay_methods(df):
+    
+    copy = df.copy()
+    
+    plt.subplot(131)
+    plt.pie(copy.paymentmethod.value_counts(), labels = ['E-Check', 'Mail Check' , 'Bank Transfer (Auto)', 'Credit Card (Auto)'], autopct = '%.1f%%', textprops = {'fontsize':20, 'fontweight':'bold'}, startangle = -90)
+    plt.title('Customer Payment Method Composition', fontsize = 30, fontweight = 'bold')
+    
+    plt.subplot(132)
